a61601a936e169e04a8a98a70621437e774cbe95
Dimas Latief <latief.selatan@gmail.com>
2018-03-05 11:21:48 +0700

    employee table structure and data insert

--- assets/admin.py
+++ assets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Assets, Departments
+from .models import Assets, Departments, Employee
 
 admin.site.register(Assets)
 admin.site.register(Departments)
+admin.site.register(Employee)
--- assets/models.py
+++ assets/models.py
@@ -18,3 +18,13 @@ class Departments(models.Model):
 
     class Meta:
         verbose_name_plural = "Departments"
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=20)
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    # boss = models.ForeignKey("self", on_delete=models.CASCADE, default='')
+    boss = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.employee_name
